1889784dad8079ec46fe084acc927354a6f2b3ab
guoguo <guoguo@guoguos-MacBook-Pro.local>
2019-06-20 18:17:41 -0400

    change cors

--- src/main/java/com/example/shoppingcart/controllers/CartController.java
+++ src/main/java/com/example/shoppingcart/controllers/CartController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.websocket.server.PathParam;
 import java.sql.SQLException;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins =  "*" )
 @RestController
 public class CartController {
     JDBCCreateTable db = new JDBCCreateTable();
